b2b37845ca6bea8fe47f9d9cd771200f9b827d5d
Daniel Østerballe <losmobilos3@gmail.com>
2024-04-25 10:18:25 +0200

    Made writeToDB conform with HEUCOD

--- writeToDB/index.php
+++ writeToDB/index.php
@@ -7,12 +7,13 @@
     }
 
     # $_GET[i] gets the parameters given in the URL
-    #//! Made into a POST request instead
-    // Takes raw data from the request
+    # Takes raw data from the request
     $json = file_get_contents('php://input');
+    #echo $json;
     $data = json_decode($json);
-    $ts = $data->timeStamp;
+    $ts = $data->timestamp;
     $et = $data->eventType;
+    
 
     echo "Received timeStamp: $ts, eventType: $et\n";
 
